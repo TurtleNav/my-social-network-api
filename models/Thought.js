54498547857ead816b0335eef60c4de2aad7e2b8
TurtleNav <tourangeau_luc@yahoo.com>
2024-01-25 11:54:01 -0500

    implemented reactionCount virtual for thought model

--- models/Thought.js
+++ models/Thought.js
@@ -2,7 +2,6 @@ const { Schema, model } = require('mongoose');
 
 const reactionSchema = require('./Reaction');
 
-
 const thoughtSchema = new Schema({
 	thoughtText: {type: String, required: true, minLength: 1, maxLength: 280},
 
@@ -12,9 +11,19 @@ const thoughtSchema = new Schema({
 	username: {type: String, required: true},
 
 	reactions: [reactionSchema]
+},
+{
+	// Necessary to specify to Mongoose that we want virtuals to be supported
+	toJSON: {
+		virtuals: true
+	}
 });
 
-// TODO: Implement a virtual called reactionCount that retrieves the length of the thought's reactions array field on query
+thoughtSchema.virtual('reactionCount').get(
+	function() {
+    return this.reactions.length;
+	}
+);
 
 const Thought = model('thought', thoughtSchema);
 
