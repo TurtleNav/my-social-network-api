0eb0d727423da0ca841cfae7bb59c101c63c9434
TurtleNav <tourangeau_luc@yahoo.com>
2024-01-26 10:18:59 -0500

    Working on user controller, some functions are dummy

---
+++ controllers/userController.js
@@ -0,0 +1,119 @@
+const { ObjectId } = require('mongoose').Types;
+const { User, Thought } = require('../models');
+
+
+async function getUsers(req, res) {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+}
+
+async function getSingleUser(req, res) {
+  try {
+    const user = await User.findOne({_id: req.params.studentId});
+      //.select('-__v')
+      //.lean();
+    if (!user) {
+      res.status(404).json({message: 'There aren\'t any users with that id'});
+    }
+    res.status(200).json({user});
+  } catch(err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+}
+
+async function createUser(req, res) {
+  try {
+    const user = await User.create(req.body);
+    res.status(200).json({user});
+  } catch(err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+}
+
+async function deleteUser(req, res) {
+  try {
+    const user = await User.findOneAndRemove({_id: req.params.userId});
+
+    if (!user) {
+      res.status(404).json({message: "There isn\'t any user with that id"});
+    }
+
+    /*
+      TODO: implement some code below that duplicates the commented out behavior.
+      Must deleted thoughts associated with a user instead
+
+      const course = await Course.findOneAndUpdate(
+        { students: req.params.studentId },
+        { $pull: { students: req.params.studentId } },
+        { new: true }
+      );
+
+      if (!course) {
+        return res.status(404).json({
+          message: 'Student deleted, but no courses found',
+        });
+      }
+
+    */
+    res.status(200).json({message: 'User was successfully deleted'});
+  } catch(err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+}
+
+async function addThought(req, res) {
+  try {
+    console.log('Adding a thought to the user');
+    console.log(req.body);
+    const user = await User.findOneAndUpdate(
+      {_id: req.params.userId},
+      { $addToSet: {thoughts: req.body}},
+      {runValidators: true, new: true}
+    );
+
+    if (!user) {
+      res.status(404).json({message: ''})
+    }
+
+  } catch(err) {
+    res.status(500).json(err);
+  }
+}
+
+async function removeThought(req, res) {
+  /*
+  try {
+    const student = await Student.findOneAndUpdate(
+      { _id: req.params.studentId },
+      { $pull: { assignment: { assignmentId: req.params.assignmentId } } },
+      { runValidators: true, new: true }
+    );
+
+    if (!student) {
+      return res
+        .status(404)
+        .json({ message: 'No student found with that ID :(' });
+    }
+
+    res.json(student);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  */
+}
+
+module.exports = {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  addThought,
+  removeThought
+};
