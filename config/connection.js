75543e7a9ee943ada102415e7898544e6da6eec2
TurtleNav <tourangeau_luc@yahoo.com>
2024-02-09 09:42:56 -0500

    Experienced some db conection errors so I added some error catching functionality

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,15 @@
 const { connect, connection } = require('mongoose');
 
-const connectionString = 'mongodb://127.0.0.1:27017/studentsDB';
-
-connect(connectionString);
+// This try-catch block handles any MongoDB connection
+// errors on startup
+async function connectDB() {
+  try {
+    await connect('mongodb://127.0.0.1:27017/mysnDB');
+  } catch(err) {
+    console.error(`Mongoose failed to connect on startup and raised:\n${err}`);
+  }
+}
+// Connect to DB
+connectDB();
 
 module.exports = connection;
